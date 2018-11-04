555fc68c959a9e8ec0e0da7d603a0f9570d2e52a
Volkan Unsal <spocksplanet@gmail.com>
2018-11-04 11:21:31 -0500

    More spec refactoring

--- spec/security/system_calls_spec.rb
+++ spec/security/system_calls_spec.rb
@@ -1,31 +1,17 @@
 require 'spec_helper'
 
-describe RubySandbox::Sandbox, 'RubySandbox sandbox' do
-  it 'should raise SecurityError when try to run shell cmd with backsticks' do
-    priv = RubySandbox::Whitelist.new
-    expect do
-      RubySandbox::Sandbox.new.run('`ls`', priv)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'should raise SecurityError when try to run shell cmd with percent' do
-    priv = RubySandbox::Whitelist.new
-    expect do
-      RubySandbox::Sandbox.new.run('%x[ls]', priv)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'should raise SecurityError when try to run shell cmd by calling system method' do
-    priv = RubySandbox::Whitelist.new
-    expect do
-      RubySandbox::Sandbox.new.run("system('ls')", priv)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'should raise SecurityError when try to run shell cmd by calling exec method' do
-    priv = RubySandbox::Whitelist.new
-    expect do
-      RubySandbox::Sandbox.new.run("exec('ls')", priv)
-    end.to raise_error(SecurityError)
+describe RubySandbox::Sandbox do
+  let(:priv) { RubySandbox::Whitelist.new }
+  [
+    '`ls`',
+    '%x[ls]',
+    "system('ls')",
+    "exec('ls')"
+  ].each do |code|
+    describe code do
+      let(:action) { described_class.new.run(code, priv) }
+      subject { -> { action } }
+      it { is_expected.to raise_error(SecurityError) }
+    end
   end
 end
