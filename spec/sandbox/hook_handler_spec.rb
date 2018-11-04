3d4a38f89be5c734c2ddb9af0ceaa2a232a7e957
Volkan Unsal <spocksplanet@gmail.com>
2018-11-04 11:48:47 -0500

    Refactor specs

--- spec/sandbox/hook_handler_spec.rb
+++ spec/sandbox/hook_handler_spec.rb
@@ -3,169 +3,78 @@ require 'evalhook'
 
 include RubySandbox
 
-describe 'Sandbox#hook_handler' do
-  it 'be obtainable from sandbox' do
-    Sandbox.new.hook_handler
+describe 'Sandbox' do
+  let(:sandbox) { Sandbox.new }
+  let(:privileges) { Whitelist.new }
+
+  describe '#hook_handler' do
+    subject { sandbox }
+    it { is_expected.to respond_to(:hook_handler) }
   end
 
-  it 'be obtainable from sandbox through create_hook_handler' do
-    sandbox = Sandbox.new
-    hook_handler = sandbox.create_hook_handler
-    expect(hook_handler).to be_kind_of(EvalHook::HookHandler)
+  describe '#create_hook_handler' do
+    let(:hook_handler) { sandbox.create_hook_handler }
+    subject { hook_handler }
+    it { is_expected.to be_kind_of(EvalHook::HookHandler) }
   end
 
   class X
     def foo; end
   end
-  it 'raises SecurityError when handle calls without privileges' do
-    sandbox = Sandbox.new
-    hook_handler = sandbox.create_hook_handler
+  ::A = nil
 
-    x = X.new
-    expect do
-      hook_handler.handle_method(X, x, :foo)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'does not raise SecurityError with method privileges' do
-    sandbox = Sandbox.new
-    priv = Whitelist.new
-    priv.allow_method(:foo)
-
-    hook_handler = sandbox.create_hook_handler(privileges: priv, source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
+  describe do
+    before do
+      allow(hook_handler).to receive(:get_caller).and_return('test-source')
     end
+    let(:hook_handler) { sandbox.create_hook_handler(opts) }
+    let(:opts) { unprivileged_opts }
+    let(:privileged_opts) { { privileges: privileges, source: 'test-source' } }
+    let(:unprivileged_opts) { { source: 'test-source' } }
+    subject { -> { action }}
 
-    x = X.new
-    expect do
-      hook_handler.handle_method(X, x, :foo)
-    end.to_not raise_error
-  end
+    [
+      [
+        'method calls',
+        proc { hook_handler.handle_method(X, X.new, :foo) },
+        proc { privileges.allow_method(:foo) }
+      ],
+      [
+        'handle_gasgn',
+        proc { hook_handler.handle_gasgn(:$a, nil) },
+        proc { privileges.allow_global_write(:$a) }
+      ],
+      [
+        'handle_cdecl',
+        proc { hook_handler.handle_cdecl(Object, :A, nil) },
+        proc { privileges.allow_const_write('Object::A') }
+      ],
+      [
+        'handle_gvar',
+        proc { hook_handler.handle_gvar(:$a) },
+        proc { privileges.allow_global_read(:$a) }
+      ],
+      [
+        'handle_const',
+        proc { hook_handler.handle_const(:A) },
+        proc { privileges.allow_const_read('A') }
+      ],
+    ].each do |name, action_code, privilege_code|
+      describe name do
+        let(:action) { instance_eval(&action_code) }
 
-  it 'raises SecurityError with handle_gasgn without privileges' do
-    sandbox = Sandbox.new
+        describe 'without privileges' do
+          it { is_expected.to raise_error(SecurityError) }
+        end
 
-    hook_handler = sandbox.create_hook_handler(source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
+        describe 'with privileges' do
+          let(:opts) { privileged_opts }
+          before do
+            instance_eval(&privilege_code)
+          end
+          it { is_expected.to_not raise_error }
+        end
+      end
     end
-
-    expect do
-      hook_handler.handle_gasgn(:$a, nil)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'does not raise SecurityError with handle_gasgn with privileges' do
-    sandbox = Sandbox.new
-    privileges = Whitelist.new
-
-    privileges.allow_global_write(:$a)
-
-    hook_handler = sandbox.create_hook_handler(privileges: privileges, source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_gasgn(:$a, nil)
-    end.to_not raise_error
-  end
-
-  it 'raises SecurityError with handle_cdecl without privileges' do
-    sandbox = Sandbox.new
-
-    hook_handler = sandbox.create_hook_handler(source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_cdecl(Object, :A, nil)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'does not raise SecurityError with handle_cdecl with privileges' do
-    sandbox = Sandbox.new
-    privileges = Whitelist.new
-
-    privileges.allow_const_write('Object::A')
-
-    hook_handler = sandbox.create_hook_handler(privileges: privileges, source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_cdecl(Object, :A, nil)
-    end.to_not raise_error
-  end
-
-  it 'raises SecurityError with handle_gvar without privileges' do
-    sandbox = Sandbox.new
-
-    hook_handler = sandbox.create_hook_handler(source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_gvar(:$a)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'does not raise SecurityError with handle_gasgn with privileges' do
-    sandbox = Sandbox.new
-    privileges = Whitelist.new
-
-    privileges.allow_global_read(:$a)
-
-    hook_handler = sandbox.create_hook_handler(privileges: privileges, source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_gvar(:$a)
-    end.to_not raise_error
-  end
-
-  it 'raises SecurityError with handle_const without privileges' do
-    sandbox = Sandbox.new
-
-    hook_handler = sandbox.create_hook_handler(source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_const(:A)
-    end.to raise_error(SecurityError)
-  end
-
-  it 'does not raise SecurityError with handle_cdecl with privileges' do
-    sandbox = Sandbox.new
-    privileges = Whitelist.new
-
-    ::A = nil
-    privileges.allow_const_read('A')
-
-    hook_handler = sandbox.create_hook_handler(privileges: privileges, source: 'test-source')
-
-    def hook_handler.get_caller
-      'test-source'
-    end
-
-    expect do
-      hook_handler.handle_const(:A)
-    end.to_not raise_error
   end
 end
